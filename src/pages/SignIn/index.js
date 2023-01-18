fe7ef18dea79b889105812bff4e584bacd52b4d2
junior <juniormathiias@gmail.com>
2023-01-18 11:12:55 -0300

    just finished the login and register pages, styled already

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,44 @@
 import * as S from './styles';
 import { useState } from 'react';
+//import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+function handleSubmit(e){
+  e.preventDefault();
+  alert('Please enter');
+}
+
   return (
     <S.ContainerCenter>
       <S.Login>
-        <S.LogoArea>
-          <img src={logo} alt="Logo System"/>
-        </S.LogoArea>
+        <S.LoginArea>
+          <S.MyIcon src={logo} alt="Logo System" />
+        </S.LoginArea>
+        <S.Form onSubmit={handleSubmit}>
+          <S.Title>Sign In</S.Title>
+          <S.FormInput
+            type="text"
+            placeholder="Type your email"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value)} 
+          />
+          <S.FormInput
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value)} 
+          />
+          <S.Button
+            type="submit"
+          >
+            Sign In
+          </S.Button>
+        </S.Form>
+        <S.Link to="/register">Don't have an account? Subscribe</S.Link>
       </S.Login>
     </S.ContainerCenter>
   );
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,53 @@
+import * as S from './styles';
+import { useState } from 'react';
+//import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 function SignUp() {
+  const [ name, setName ] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+function handleSubmit(e){
+  e.preventDefault();
+  alert('Please enter');
+}
+
   return (
-    <div>
-      <h1>TELA DE CADASTRO</h1>
-    </div>
+    <S.ContainerCenter>
+      <S.Login>
+        <S.LoginArea>
+          <S.MyIcon src={logo} alt="Logo System" />
+        </S.LoginArea>
+        <S.Form onSubmit={handleSubmit}>
+          <S.Title>Create Account</S.Title>
+          <S.FormInput
+            type="text"
+            placeholder="Type your name"
+            value={name}
+            onChange={ (e) => setName(e.target.value)} 
+          />
+          <S.FormInput
+            type="text"
+            placeholder="Type your email"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value)} 
+          />
+          <S.FormInput
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value)} 
+          />
+          <S.Button
+            type="submit"
+          >
+            Register
+          </S.Button>
+        </S.Form>
+        <S.Link to="/">Already have account ? Sign In</S.Link>
+      </S.Login>
+    </S.ContainerCenter>
   );
 }
 
