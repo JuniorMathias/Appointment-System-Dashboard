68f132f05b53f1589ff07ac29ae306211d49dd2c
junior <juniormathiias@gmail.com>
2023-01-24 19:57:04 -0300

    just finished for today tomorrow i will be back and added some styles

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -6,14 +6,16 @@ export const Content = styled.div`
 
 export const Container = styled.div`
     display: flex;
-    background-color: #f8f8f8;
+    background-color: #cecece;
     border-radius: 5px;
     padding: .8em;
     align-items: center;
     margin-left: 200px;
 `;
 export const Form = styled.form`
-    
+    margin-bottom: 1em;
+    border-radius: 50%;
+    object-fit: cover;
 `;
 
 export const LabelAvatar = styled.label`
@@ -26,7 +28,8 @@ export const LabelAvatar = styled.label`
     cursor: pointer;
 `;
 export const Label = styled.label`
-    
+    margin-bottom: 0.5em;
+    font-size: 1.4em;
 `;
 
 export const Span = styled.span`
@@ -36,6 +39,7 @@ export const Span = styled.span`
     transition: all 0.5s;
     &:hover {
         opacity: 1;
+        transform: scale(1.4);
     }
 `;
 export const Button = styled.button`
@@ -44,5 +48,10 @@ export const Button = styled.button`
 
 export const FormInput = styled.input`
     display: none;
+    margin-bottom: 1em;
+    padding: 0.7em;
+    border: 0;
+    border-radius: 5px;
+    max-width: 600px;
 `;
 
