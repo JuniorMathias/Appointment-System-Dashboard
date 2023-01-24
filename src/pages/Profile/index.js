6875407636cc9bd6ff01fc7f8c7b35b39d7efc5c
junior <juniormathiias@gmail.com>
2023-01-23 22:58:12 -0300

    just started the profile page with some files and pic

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,20 @@
+import { useState, useContext } from 'react';
 import * as S from './styles';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../contexts/auth';
 
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings,FiUpload } from 'react-icons/fi';
+
 
 export default function Profile(){
+  const { user } = useContext(AuthContext);
+
+  const [name, setName] = useState(user && user.name);
+  const [email, setEmail] = useState(user && user.email);
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
   return(
       <>
         <Header />
@@ -12,6 +22,24 @@ export default function Profile(){
             <Title name="My Profile">
                 <FiSettings size={25} />
             </Title>
+            <S.Container>
+              <S.Form>
+                <S.LabelAvatar>
+                  <S.Span>
+                    <FiUpload color='#fff' size={25} />
+                  </S.Span>
+                  <S.FormInput
+                    type="file"
+                    accept="image/*"
+                  />
+                  { avatarUrl == null ? 
+                    <img src={avatar} width="250" height="250" alt="profile pic" />
+                     :
+                     <img src={avatarUrl} width="250" height="250" alt="profile pic" />
+                  }
+                </S.LabelAvatar>
+              </S.Form>
+            </S.Container>
         </S.Content>
       </>
   )
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -3,3 +3,22 @@ import styled from 'styled-components';
 export const Content = styled.div`
     
 `;
+
+export const Container = styled.div`
+    margin-left: 250px;
+`;
+export const Form = styled.form`
+    
+`;
+
+export const LabelAvatar = styled.div`
+    
+`;
+
+export const Span = styled.span`
+    
+`;
+export const FormInput = styled.input`
+    
+`;
+
