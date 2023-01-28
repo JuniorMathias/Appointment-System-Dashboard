275aa4273c070d45d0b520571b83778f3f17273d
junior <juniormathiias@gmail.com>
2023-01-28 00:50:20 -0300

    finished the costumer page easy because the styles is already done, and just some inputs with values

--- src/pages/Costumers/index.js
+++ src/pages/Costumers/index.js
@@ -1,17 +1,59 @@
 
-import { useContext } from 'react';
+import { useState } from 'react';
 import * as S from './styles';
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiUser } from 'react-icons/fi';
+
+
 
 export default function Costumers(){
-  //here is gettin signOut from authContext there is a conditional
-  const { signOut } = useContext(AuthContext);
+  const [fakeName, setFakeName] = useState('');
+  const [document, setDocument] = useState('');
+  const [address, setAddress] = useState('');
+
+  function handleAdd(e){
+    e.preventDefault();
+  }
 
   return(
-    <div>
-      <Header />
-      <h1>PAGINA DASHBOARD</h1>
-    </div>
+      <div>
+        <Header />
+        <S.Content>
+            <Title name="Customers">
+                <FiUser size={25} />
+            </Title>
+        <S.Container>
+            <S.Form onSubmit={handleAdd}>
+                <S.Label>Fake Name</S.Label>
+                <S.FormInput
+                    type="text"
+                    value={fakeName}
+                    placeholder="Company Name"
+                    onChange={ (e) => setFakeName(e.target.value)} 
+                />
+
+                <S.Label>Document Number</S.Label>
+                <S.FormInput
+                    type="text"
+                    value={document}
+                    placeholder="Document Number"
+                    onChange={ (e) => setDocument(e.target.value)} 
+                />
+
+                <S.Label>Address</S.Label>
+                <S.FormInput
+                    type="text"
+                    value={address}
+                    placeholder="Company Address"
+                    onChange={ (e) => setAddress(e.target.value)} 
+                />
+                <S.Button type='submit'>Register</S.Button>
+            </S.Form>
+        </S.Container>
+        </S.Content>
+      </div>
+      
   )
 }
--- src/pages/Costumers/styles.js
+++ src/pages/Costumers/styles.js
@@ -1,1 +1,62 @@
 import styled from 'styled-components';
+
+export const Content = styled.div`
+`;
+
+export const Container = styled.div`
+    display: flex;
+    background-color: #dcdcdc;
+    border-radius: 5px;
+    padding: .8em;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 1em;
+    margin-left: 10%;
+    @media screen and (max-width: 750px){
+        margin-left: 0;
+        margin-top: 0;
+        margin-bottom: 1em;
+}
+`;
+
+export const Form = styled.form`
+    margin-bottom: 1em;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-bottom: 1em;
+    padding: .7em;
+    border:0;
+    @media screen and (max-width: 700px){
+        margin-left: 0;
+    }
+`;
+
+export const Label = styled.label`
+    margin-bottom: 0.5em;
+    font-size: 1.4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+export const FormInput = styled.input`
+    margin-bottom: 1em;
+    margin-top: 0.5em;
+    padding: .7em;
+    padding-right: 10rem;
+    border:0;
+    border-radius: 5px;
+    max-width: 600px;
+`;
+export const Button = styled.button`
+    padding: 8px 40px 8px 40px;
+    background-color: #121212;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 1.4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 30%;
+`;
